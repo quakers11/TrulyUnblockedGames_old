1b6dd73b4f1f9790733ac3bf84fe08c56db639d0
Wade7wastaken <david@thecallenders.com>
2023-02-13 07:31:39 -0600

    update minify.py

--- Scripts/minify.py
+++ Scripts/minify.py
@@ -1,5 +1,9 @@
 import os
 import shutil
+import minify_html
+import json
+import requests
+import cssmin
 
 shutil.rmtree('Output')
 
@@ -12,9 +16,8 @@ except FileExistsError:
 for subdir, dirs, files in os.walk("./"):
     for file in files:
         f = os.path.normpath(os.path.join(subdir, file))
-        
 
-        #Check if the file starts with a .
+        # Check if the file starts with a .
         skip = False
         for folder in f.split(os.sep):
             if folder.startswith("."):
@@ -30,10 +33,49 @@ for subdir, dirs, files in os.walk("./"):
             continue
         if "EmulatorJS" in f and "minify" in f:
             continue
-        
-        with open(f, "rb") as o:
-            content = o.read()
-            outpath = os.path.join("Output", f)
-            os.makedirs(os.path.dirname(outpath), exist_ok=True)
-            with open(outpath, "wb") as output:
-                output.write(content)
+
+        print("Processing: " + f)
+
+        if os.path.splitext(f)[1] == ".html":
+            with open(f, "rt", encoding="utf-8") as o:
+                content = minify_html.minify(
+                    o.read(), do_not_minify_doctype=True, ensure_spec_compliant_unquoted_attribute_values=True, minify_css=True, minify_js=True)
+                
+                outpath = os.path.join("Output", f)
+                os.makedirs(os.path.dirname(outpath), exist_ok=True)
+                with open(outpath, "wt", encoding="utf-8") as output:
+                    output.write(content)
+        elif os.path.splitext(f)[1] == ".json":
+            with open(f, "rt", encoding="utf-8") as o:
+                content = json.dumps(json.loads(o.read()), separators=(',', ':'))
+                
+                outpath = os.path.join("Output", f)
+                os.makedirs(os.path.dirname(outpath), exist_ok=True)
+                with open(outpath, "wt", encoding="utf-8") as output:
+                    output.write(content)
+        elif os.path.splitext(f)[1] == ".js":
+            with open(f, "rt", encoding="utf-8") as o:
+                content = requests.post("https://www.toptal.com/developers/javascript-minifier/api/raw", data={
+                    "input": o.read()
+                }).text
+
+                outpath = os.path.join("Output", f)
+                os.makedirs(os.path.dirname(outpath), exist_ok=True)
+                with open(outpath, "wt", encoding="utf-8") as output:
+                    output.write(content)
+        elif os.path.splitext(f)[1] == ".css":
+            with open(f, "rt", encoding="utf-8") as o:
+                content = cssmin.cssmin(o.read())
+
+                outpath = os.path.join("Output", f)
+                os.makedirs(os.path.dirname(outpath), exist_ok=True)
+                with open(outpath, "wt", encoding="utf-8") as output:
+                    output.write(content)
+        else:
+            with open(f, "rb") as o:
+                content = o.read()
+
+                outpath = os.path.join("Output", f)
+                os.makedirs(os.path.dirname(outpath), exist_ok=True)
+                with open(outpath, "wb") as output:
+                    output.write(content)
